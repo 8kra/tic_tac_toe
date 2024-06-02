76e013c6c87694a8b444a2c7ff630949ac7134bd
8kra <ifrahh121@gmail.com>
2024-06-02 15:00:46 +0300

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe Game In React JS
+
+![tic tac toe](https://github.com/8kra/tic_tac_toe/assets/115061491/23ffc4c2-4073-4534-9ddd-f6f58ab052e2)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[500px]">
+        <Board />
+      </div>
+    </div>
+  );
+}
+
+export default App;
